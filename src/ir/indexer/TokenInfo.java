34f055deed5b5f3c6eb5ae80b39d8ef82633970d
blouke <muhammad.atif@live.co.uk>
2016-02-23 17:13:23 -0600

    Update TokenInfo.java

--- src/ir/indexer/TokenInfo.java
+++ src/ir/indexer/TokenInfo.java
@@ -13,7 +13,7 @@ public class TokenInfo {
 	}
 	
 	public void calculateIdf(int totalDoc){
-		idf = Math.log(occMap.size())/Math.log(totalDoc);
+		idf = Math.log(totalDoc/occMap.size())/Math.log(totalDoc);
 		System.out.println("Document frequency: "+occMap.size());		//remove this line
 	}
 	
